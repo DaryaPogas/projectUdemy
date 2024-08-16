2551a9e10415ce055cc41f582e1b41d21c51be7b
Darya <daryapogas@gmail.com>
2024-08-16 17:38:16 -0500

    второе задание по проекту

--- code.js
+++ code.js
@@ -6,14 +6,31 @@ let personalMovieDB = {
   genres: [],
   privat: false,
 };
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+
+for (let i = 0; i < 2; i++){
+  const a = prompt("Один из последних просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
+  if (a != '' && b !='' && a.length < 50 && a != null && b != null && b.length < 50){
+    personalMovieDB.movies[a] = b
+    console.log ('done');
+  }
+    else {
+      console.log ('error');
+      i--;
+    }
+}
 console.log(personalMovieDB);
 
+if (personalMovieDB.count < 10) {
+  console.log ('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB >= 10 && personalMovieDB.count <= 30) {
+  console.log ('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+  console.log ('Вы киноман')
+} else {
+  console.log ('Произошла ошибка')
+}
 
+console.log(personalMovieDB);
